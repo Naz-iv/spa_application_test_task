d912fd050298b1b2c269c073b561be7af5fe7be1
naz-iv <nazarivankiv1@gmail.com>
2023-12-27 20:46:33 +0200

    prepared tests for models and custom validation / configured admin panel

--- spa_comments/admin.py
+++ spa_comments/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from spa_comments.models import Comment, Reply, User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
+
+
+admin.site.register(Comment)
+admin.site.register(Reply)
+
+admin.site.unregister(Group)
--- spa_comments/models.py
+++ spa_comments/models.py
@@ -13,45 +13,58 @@ class User(AbstractUser):
     email = models.EmailField()
     profile_image = models.ImageField(null=True, blank=True)
 
-
-def validate_text(text: str) -> str:
-    """Validates that all HTML tags were closed correctly and permitted HTML tags used"""
-    print("validating text")
-    allowed_tags = ("a", "code", "i", "strong")
-
-    tags_deque = deque()
-
-    tags = re.findall(r"<[^>]*>", text)
-
-    for tag in tags:
-        print("Tag:", tag)
-        if "/" not in tag:
-            if tag.split().strip("<").strip(">")[0] not in allowed_tags:
-                raise ValidationError(
-                    "Unsupported HTML tag used! Only following HTML tags are allowed: "
-                    "<a></a>, <code></code>, <i></i>, <strong></strong>"
-                )
-            tags_deque.append(tag)
-        else:
-            if not tags_deque:
-                raise ValidationError("Closed tags must match opening tags!")
-            opening_tag = tags_deque.pop()
-            if opening_tag[1:] != tag[2:-1]:
-                raise ValidationError("Opening tags must match closing tags!")
-
-    if len(tags_deque) != 0:
-        raise ValidationError("All opened HTML tags must be closed!")
-
-    return text
+    def __str__(self):
+        return self.username
 
 
 class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="comments")
     published_at = models.DateTimeField(auto_now_add=True)
-    text = models.TextField(validators=[MinLengthValidator(1), validate_text])
+    text = models.TextField(validators=[MinLengthValidator(1)])
 
     class Meta:
         ordering = ['-published_at']
+
+    def clean(self):
+        super().clean()
+
+        allowed_tags = (
+            "<a>", "</a>", "<code>", "</code>", "<i>", "</i>", "<strong>", "</strong>"
+        )
+
+        tags_deque = deque()
+
+        tags = re.findall(r"<[^>]*>", self.text)
+
+        for tag in tags:
+            if "/" not in tag:
+                if tag.startswith("<a "):
+                    tag = tag.split()[0] + ">"
+                tags_deque.append(tag)
+            else:
+                if not tags_deque:
+                    raise ValidationError("Closed tags must match opening tags!")
+
+                if tag not in allowed_tags:
+                    raise ValidationError(
+                        "Unsupported HTML tag used! Only following HTML tags are allowed: "
+                        "<a></a>, <code></code>, <i></i>, <strong></strong>"
+                    )
+
+                opening_tag = tags_deque.pop()
+                if opening_tag[1:] != tag[2:]:
+                    raise ValidationError("Opening tags must match closing tags!")
+
+        if len(tags_deque) != 0:
+            raise ValidationError("All opened HTML tags must be closed!")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        formatted_time = self.published_at.strftime("%H:%M:%S %d-%m-%Y")
+        return f"Comment by {self.user} at {formatted_time}"
 
 
 class Reply(models.Model):
