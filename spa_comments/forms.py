6f37556ddff4975b3078a0e2eef67c2322fed178
naz-iv <nazarivankiv1@gmail.com>
2023-12-29 00:12:20 +0200

    added reply creation form

--- spa_comments/forms.py
+++ spa_comments/forms.py
@@ -1,7 +1,7 @@
 from captcha.fields import CaptchaField
 from django import forms
 
-from spa_comments.models import Comment
+from spa_comments.models import Comment, Reply
 
 
 class CommentCreateForm(forms.ModelForm):
@@ -29,3 +29,34 @@ class CommentCreateForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["username", "email", "text", "captcha"]
+
+
+class ReplyCreateForm(forms.ModelForm):
+    original_comment_id = forms.IntegerField(
+        label="Comment ID",
+        widget=forms.HiddenInput()
+    )
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter your username..."}),
+        required=True,
+    )
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "Enter your email..."}),
+        required=True,
+    )
+
+    text = forms.CharField(
+        widget=forms.Textarea(attrs={"class": "textarea", "placeholder": "Enter your comment..."}),
+        required=True,
+    )
+
+    captcha = CaptchaField(
+        label="Captcha",
+        help_text="Enter the captcha"
+    )
+
+    class Meta:
+        model = Reply
+        fields = ["username", "email", "text", "captcha", "original_comment_id"]
