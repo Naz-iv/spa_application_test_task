befa80c21db4b74ec8f97c2b1844977000b66592
naz-iv <nazarivankiv1@gmail.com>
2023-12-30 20:39:31 +0200

    updated comments caching logic and added signal for updating cache every time new comment is added

--- spa_comments/apps.py
+++ spa_comments/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SpaCommentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "spa_comments"
+
+    def ready(self):
+        import spa_comments.signals
--- spa_comments/utils.py
+++ spa_comments/utils.py
@@ -1,13 +1,33 @@
 import random
 import string
 
+from django.core.cache import cache
+from django.utils import timezone
+
+from spa_comments.models import Comment
+
 
 def generate_random_alphanumeric_challenge(length=6):
     challenge = u""
     response = u""
-    characters = string.ascii_letters + string.digits
+    characters = string.ascii_uppercase + string.digits
+
     for _ in range(length):
         digit = random.choice(characters)
         challenge += digit
         response += digit
+
     return challenge, response
+
+
+def get_expensive_data():
+    """Reads all comments from Database and saves them to cache"""
+
+    data = cache.get("comments_cache")
+
+    if data is None:
+        data = Comment.objects.filter(parent_comment__isnull=False)
+
+        cache.set("comments_cache", data, timeout=3600)
+
+    return data
